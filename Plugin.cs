8b7749cb2887cf716c8202ad5455c784f8880dee
tairasoul <mjrcoetzee+1@outlook.com>
2023-12-28 14:51:18 +0200

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,141 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MainMenuUIExtension
+{
+    [BepInPlugin("tairasoul.mainmenu.uiextension", "MainMenuExtension", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal void Awake()
+        {
+            Logger.LogInfo("MainMenuExtension awake.");
+        }
+
+        internal KeyboardTrigger.Key FindButtonByKey(KeyCode key)
+        {
+            GameObject baseCanvas = GameObject.Find("Canvas/Image");
+            KeyboardTrigger trigger = baseCanvas.GetComponent<KeyboardTrigger>();
+            foreach (KeyboardTrigger.Key KeyTrigger in trigger.Choices)
+            {
+                foreach (GameObject obj in baseCanvas.GetChildren())
+                {
+                    Text text = obj.GetComponent<Text>();
+                    if (text.text.StartsWith(key.ToString()))
+                    {
+                        return KeyTrigger;
+                    }
+                }
+            }
+            return null;
+        }
+
+        internal KeyboardTrigger.Key FindButtonByKey(string key)
+        {
+            GameObject baseCanvas = GameObject.Find("Canvas/Image");
+            KeyboardTrigger trigger = baseCanvas.GetComponent<KeyboardTrigger>();
+            foreach (KeyboardTrigger.Key KeyTrigger in trigger.Choices)
+            {
+                foreach (GameObject obj in baseCanvas.GetChildren())
+                {
+                    Text text = obj.GetComponent<Text>();
+                    if (text.text.StartsWith(key))
+                    {
+                        return KeyTrigger;
+                    }
+                }
+            }
+            return null;
+        }
+
+        internal GameObject FindElectric()
+        {
+            foreach (ParticleSystem system in Resources.FindObjectsOfTypeAll(typeof(ParticleSystem)).Cast<ParticleSystem>())
+            {
+                if (system.name != "Dust" && system.name != "sound")
+                {
+                    return system.gameObject;
+                }
+            }
+            return null;
+        }
+
+        internal GameObject FindLoadingScreen()
+        {
+            HomeScreen screen = GameObject.FindFirstObjectByType<HomeScreen>();
+            return screen.gameObject.transform.Find("LoadingScreen").gameObject;
+        }
+
+        internal void SetupOtherButtons()
+        {
+            GameObject baseCanvas = GameObject.Find("Canvas/Image");
+            baseCanvas.transform.GetChild(0).gameObject.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                KeyboardTrigger trigger = baseCanvas.GetComponent<KeyboardTrigger>();
+                trigger.active = false;
+                try
+                {
+                    baseCanvas.SetActive(false);
+                    FindElectric().SetActive(true);
+                    FindLoadingScreen().SetActive(true);
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogError(ex);
+                }
+            });
+        }
+
+        internal void Update()
+        {
+            if (SceneManager.GetActiveScene().name == "Menu" && GameObject.Find("Canvas/Image/ModSettings") == null)
+            {
+                SetupOtherButtons();
+                GameObject baseCanvas = GameObject.Find("Canvas/Image");
+                GameObject[] defaultTexts = baseCanvas.GetChildren(3);
+                KeyboardTrigger.Key key = new()
+                {
+                    key = KeyCode.G,
+                    Event = new()
+                };
+                GameObject button = utils.CreateBaseUIButton();
+                button.name = "ModSettings";
+                button.GetComponent<Text>().text = "G/ Mod Settings";
+                button.transform.SetParent(baseCanvas.transform, false);
+                button.GetComponent<RectTransform>().anchoredPosition = new Vector2(-180.0165f, -27.2514f);
+                GameObject Pages = new("OptionPages");
+                Pages.transform.SetParent(baseCanvas.transform, false);
+                Pages.AddComponent<RectTransform>();
+                Logger.LogInfo("Created ModSettings button for main menu.");
+                KeyboardTrigger trigger = baseCanvas.GetComponent<KeyboardTrigger>();
+                key.Event.AddListener(() =>
+                {
+                    foreach (GameObject obj in defaultTexts)
+                    {
+                        obj?.SetActive(false);
+                    }
+                    button.SetActive(false);
+                    Pages.SetActive(true);
+                    trigger.active = false;
+                });
+                button.GetComponent<Button>().onClick.AddListener(() =>
+                {
+                    foreach (GameObject obj in defaultTexts)
+                    {
+                        obj?.SetActive(false);
+                    }
+                    button.SetActive(false);
+                    Pages.SetActive(true);
+                    trigger.active = false;
+                });
+                GameObject LiquidNoiseImage = GameObject.Find("Canvas/LiquidNoise");
+                LiquidNoiseImage.GetComponent<Image>().raycastTarget = false;
+                trigger.Choices = [.. trigger.Choices, key];
+            }
+        }
+    }
+}
---
+++ utils.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainMenuUIExtension
+{
+    public static class GameObjectExtensions
+    {
+        public static GameObject[] GetChildren(this GameObject @object, int maxChildren = 0)
+        {
+            int childCount = @object.transform.childCount;
+            if (maxChildren == 0) maxChildren = childCount;
+            GameObject[] children = new GameObject[childCount];
+            for (int i = 0; i < maxChildren; i++)
+            {
+                children[i] = @object.transform.GetChild(i).gameObject;
+            }
+            return children;
+        }
+    }
+    internal class utils
+    {
+        internal static Font GetFont(string name)
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            Object[] fonts = Object.FindObjectsOfTypeAll(typeof(Font));
+#pragma warning restore CS0618 // Type or member is obsolete
+            foreach (Font font in fonts.Cast<Font>())
+            {
+                if (font.name == name) return font;
+            }
+            return null;
+        }
+        internal static GameObject CreateBaseUIButton()
+        {
+            GameObject obj = new("Text");
+            Text text = obj.AddComponent<Text>();
+            obj.AddComponent<Button>();
+            text.font = GetFont("Arial");
+            text.fontSize = 30;
+            text.alignment = TextAnchor.UpperLeft;
+            text.horizontalOverflow = HorizontalWrapMode.Overflow;
+            obj.AddComponent<RectTransform>();
+            obj.AddComponent<CanvasRenderer>();
+            return obj;
+        }
+    }
+}
